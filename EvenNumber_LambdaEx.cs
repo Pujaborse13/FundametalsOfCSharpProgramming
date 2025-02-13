3cf31dc80836b3943d6f4f8b9eed54ba42782c44
Puja Borse <pujaborse13@gmail.com>
2025-02-13 16:10:03 +0530

    EvenNumber_Using LambdaExpression

--- EvenNumber_LambdaEx.cs
+++ EvenNumber_LambdaEx.cs
@@ -13,6 +13,7 @@ namespace FundamentalsOfCSharp.LambdaExpression
             List<int> input = new List<int> { 10, 15, 20, 25, 30, 35, 40 };
             List<int> evenNumbers = input.FindAll(x => x % 2 == 0);
 
+
             foreach (int num in evenNumbers)
             {
                 Console.WriteLine(num);
