f0752b61ae37c14cd7d91f383544e281dbb4628f
Puja Borse <pujaborse13@gmail.com>
2025-02-13 16:15:48 +0530

    EvenNumber_Using LambdaExpression

---
+++ MainClass.cs
@@ -0,0 +1,87 @@
+﻿using System.Globalization;
+using FundamentalsOfCSharp;
+using FundamentalsOfCSharp.LambdaExpression;
+
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Hello, Welcome To C# Programming");
+
+        //1. DisplayFriendsName
+        DisplayFriendsName myFriends1 = new DisplayFriendsName();
+        myFriends1.DisplayFriends();
+
+        DisplayFriendsName myFriends2 = new DisplayFriendsName();
+        myFriends2.DisplayFriendsNameUsingForeachLoop();
+
+        DisplayFriendsName myFriends3 = new DisplayFriendsName();
+        myFriends3.DisplayFriendsNameUsingForLoop();
+
+        DisplayFriendsName myFriends4 = new DisplayFriendsName();
+        myFriends4.DisplayFriendNameUserInput();
+
+        //-----------------------------------------------------
+
+        //2.SquareOfSumAndDifference
+
+        SquareOfSumAndDifference squareOfSumAndDifference = new SquareOfSumAndDifference();
+        squareOfSumAndDifference.SumAndDifference();
+
+        //-------------------------------------------------
+       
+
+        //3. DisplayPlace
+
+        VisitPlace visitPlace = new VisitPlace();
+        visitPlace.DisplayVisitPlaces();
+
+
+        //_________________________________________
+
+        
+        //4 ReadNumberAsAString
+
+        ReadNumberAsAString readNumberAsAString1 = new ReadNumberAsAString();
+        readNumberAsAString1.ReadNumberUsingToInt32();
+
+        ReadNumberAsAString readNumberAsAString2 = new ReadNumberAsAString();
+        readNumberAsAString2.ReadNumberUsingParse();
+
+
+        //5. UniqueCharactersString
+
+
+        UniqueCharactersString uniqueCharactersString = new UniqueCharactersString();
+        uniqueCharactersString.CheckString();
+
+
+        */
+
+        LambdaExDemo lambdaExDemo = new LambdaExDemo();
+        lambdaExDemo.LambdaExpressionDemo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+
