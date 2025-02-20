0444477eb75f733faf0aed5d1c0a8822c1be8c72
Puja Borse <pujaborse13@gmail.com>
2025-02-20 23:47:22 +0530

    Class Main Added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Regex_DemoProject;
+
+public class Program
+{
+    static void Main()
+    {
+
+        RegexDemo regexDemo = new RegexDemo();
+        regexDemo.DemoEx();
+
+
+    }
+}
