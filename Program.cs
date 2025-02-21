a0cee5e7d7af9127abf4bcd9abdac2374e55da19
Puja Borse <pujaborse13@gmail.com>
2025-02-21 17:19:15 +0530

    Reflection Demo Example

--- Program.cs
+++ Program.cs
@@ -1,73 +1,23 @@
-﻿using System.ComponentModel;
-using Delegates_Refelection;
-namespace Delegates_Refelection
-{ 
+﻿using Delegates_Refelection;
+using System;
+using System.Reflection;
 
-    public class Program
-    {
-
-            static void Main(string[] args)
-            {
-            /*
-            //Single method
-            MessagePrint msgprint = new MessagePrint(DelegatesDemo.MessagePrinting);
-            msgprint("Hello,Puja");
-
-            //Multiple Methods
-            MessagePrint msgprint = new MessagePrint(DelegatesDemo.MessagePrinting);
-            msgprint += DelegatesDemo.FilePrinting;
-            msgprint("Hello,Puja");
-            
+namespace Delegates_Refelection;
 
 
-            // ---------------------------------------------------------------
-            Dictionary<string, MenueAction> menu = new Dictionary<string, MenueAction> {
-                    {"1",ConsoleMenuSystem.ShowDate},
-                    {"2",ConsoleMenuSystem.ShowTime},
-                    {"3",ConsoleMenuSystem.Exit}       
-                };
+public class Program
+{
+     static void Main(string[] args)
+     {
 
+      //================= Reflection ============================================
+        
+        ReflectionDemo reflectionDemo = new ReflectionDemo();
+        reflectionDemo.ReflectionEx();
 
-            while (true)
-            {
-                Console.WriteLine("\nMenu:");
-                Console.WriteLine("1. Show Date");
-                Console.WriteLine("2. Show Time");
-                Console.WriteLine("3. Exit");
-                Console.Write("Enter your choice: ");
-                string choice = Console.ReadLine();
-
-                if (menu.ContainsKey(choice))
-                {
-                    menu[choice]();
-                    if (choice == "3") break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice. Please try again.");
-                }
-
-            }
-            */
-            //----------------------------------------------------------
-
-            Calculator calc = CalculatorDemo.Add;
-
-            Console.WriteLine("Addition: " + calc(5, 3)); 
-
-            calc = CalculatorDemo.Sub;
-            Console.WriteLine("Subtraction: " + calc(5, 3));
-
-
-
-
-
-
-
-
-
-        }
 
 
     }
+
+
 }
---
+++ ReflectionDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace Delegates_Refelection
+{
+    public  class ReflectionDemo
+    {
+        public void ReflectionEx()
+        {
+
+            string myName = "Puja";
+            Type nameType = myName.GetType();  
+            Console.WriteLine("Type is : " + nameType);
+            Console.WriteLine();
+            
+            Type t = typeof(ReflectionDemo);
+            Console.WriteLine(t.Assembly);
+            Console.WriteLine();
+
+
+            // get typeof Enumerable and load it to Type variable e   
+            Type e = typeof(Enumerable);
+            Console.WriteLine("Name : {0}", e.Name);
+            Console.WriteLine("Namespace : {0}", e.Namespace);
+            Console.WriteLine("Base Type : {0}", e.BaseType);
+            Console.WriteLine();
+
+
+            // get typeof String and load it to Type variable
+            Type s = typeof(String);
+            Console.WriteLine("Name : {0}", s.Name);
+            Console.WriteLine("Namespace : {0}", s.Namespace);
+            Console.WriteLine("Base Type : {0}", s.BaseType);
+
+
+
+        }
+
+    }
+}
