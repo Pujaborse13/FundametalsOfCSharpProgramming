cc55804be3b22469bd3d1e3852d8b5b4739297cc
Puja Borse <pujaborse13@gmail.com>
2025-02-21 14:40:18 +0530

    Console Menue System

---
+++ ConsoleMenuSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Refelection
+{
+    public delegate void MenueAction();
+    public class ConsoleMenuSystem
+    {
+        public static void ShowDate()
+        {
+
+            Console.WriteLine("Current Date :" + DateTime.Now.ToShortDateString());
+
+        }
+
+        public static void ShowTime()
+        {
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+        }
+
+        public static void Exit()
+        {
+            Console.WriteLine("Existing Application");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,15 +7,50 @@ namespace Delegates_Refelection
 
             static void Main(string[] args)
             {
+            /*
+            //Single method
+            MessagePrint msgprint = new MessagePrint(DelegatesDemo.MessagePrinting);
+            msgprint("Hello,Puja");
 
-                MessagePrint msgprint = new MessagePrint(DelegatesDemo.MessagePrinting);
-                msgprint += DelegatesDemo.FilePrinting;
-                msgprint("Hello,Puja");
+            //Multiple Methods
+            MessagePrint msgprint = new MessagePrint(DelegatesDemo.MessagePrinting);
+            msgprint += DelegatesDemo.FilePrinting;
+            msgprint("Hello,Puja");
+            */
+
+
+            // ---------------------------------------------------------------
+            Dictionary<string, MenueAction> menu = new Dictionary<string, MenueAction> {
+                    {"1",ConsoleMenuSystem.ShowDate},
+                    {"2",ConsoleMenuSystem.ShowTime},
+                    {"3",ConsoleMenuSystem.Exit}       
+                };
+
+
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Show Date");
+                Console.WriteLine("2. Show Time");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                if (menu.ContainsKey(choice))
+                {
+                    menu[choice]();
+                    if (choice == "3") break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
 
 
 
 
             }
+        }
 
 
     }
