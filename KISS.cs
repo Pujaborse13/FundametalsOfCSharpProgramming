e05c7eebc1f44b1b27b35bcad9676547a5bbbae5
Puja Borse <pujaborse13@gmail.com>
2025-02-24 14:41:17 +0530

    KISS Principle

---
+++ KISS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPrinciples_CSharp
+{
+    //KISS (Keep It Simple, Stupid) :  Write simple and clear code instead of over-engineering.
+    //Withoud KISS Rule
+    internal class KISS
+    {
+        class Calculator
+        {
+            public int Add(int a, int b)
+            {
+                if (a > 0 && b > 0)
+                {
+                    return a + b;
+                }
+                else if (a > 0 && b < 0)
+                {
+                    return a + b;
+                }
+                else if (a < 0 && b > 0)
+                {
+                    return a + b;
+                }
+                else
+                {
+                    return a + b;
+                }
+            }
+        }
+
+        //-----------------------------------------
+        //With KISS Rule
+        class Calculator1
+        {
+            public int Add1(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+
+
+    }
+}
